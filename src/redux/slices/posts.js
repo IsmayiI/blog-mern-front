45211d12f085245a4e68fabc60d0f879d780604b
Ismayil Ismayilov <ismayilov_ismayil_97@mail.ru>
2024-07-23 17:08:25 +0400

    add posts

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -3,7 +3,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from '../../axios'
 
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
-   const data = await axios.get('/posts')
+   const { data } = await axios.get('/posts')
    return data
 })
 
@@ -21,7 +21,21 @@ const initialState = {
 const postsSlice = createSlice({
    name: 'posts',
    initialState,
-   reducers: {}
+   reducers: {},
+   extraReducers: {
+      [fetchPosts.pending]: (state) => {
+         state.posts.items = []
+         state.posts.status = 'loading'
+      },
+      [fetchPosts.fulfilled]: (state, action) => {
+         state.posts.items = action.payload
+         state.posts.status = 'loaded'
+      },
+      [fetchPosts.rejected]: (state) => {
+         state.posts.items = []
+         state.posts.status = 'error'
+      },
+   }
 })
 
 export const postsReducer = postsSlice.reducer

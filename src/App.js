c2df31f37027f5a36b4663b9a18a03d923bfa836
Ismayil Ismayilov <ismayilov_ismayil_97@mail.ru>
2024-07-24 18:02:59 +0400

    add fetch auth me

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import Container from "@mui/material/Container";
 import { Header } from "./components";
 import { Home, FullPost, Registration, AddPost, Login } from "./pages";
 import { Route, Routes } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchAuthMe } from "./redux/slices/auth";
 
 function App() {
+   const dispatch = useDispatch()
+   const isAuth = useSelector(state => !!state.auth.data)
+
+   useEffect(() => {
+      dispatch(fetchAuthMe())
+   }, [])
+
    return (
       <>
          <Header />

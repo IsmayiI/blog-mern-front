b4aa58f81da2de248651170418f23f5e55a94dbb
Ismayil Ismayilov <ismayilov_ismayil_97@mail.ru>
2024-08-06 16:35:49 +0400

    change base url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-   baseURL: 'http://localhost:4444'
+   baseURL: 'https://blog-mern-back-jdr0.onrender.com'
 })
 
 instance.interceptors.request.use((config) => {
